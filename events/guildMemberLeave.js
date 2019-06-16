92f43d6c9fa39d739d54010bccb28d137e6966e5
transiSciences <37506275+transiSciences@users.noreply.github.com>
2019-06-16 09:56:59 +0200

    Init event Leave

---
+++ events/guildMemberLeave.js
@@ -0,0 +1,3 @@
+module.exports = (guild, member) => {
+  guild.channels.get('573138918044598282').send(`${member.name} vient de quitter le site... Faîtes en sorte qu'il ne parle plus`);
+}
--- launcher/eventLauncher.js
+++ launcher/eventLauncher.js
@@ -2,5 +2,6 @@ const reqEvent = (event) => require(`../events/${event}.js`);
 
 module.exports = Client => {
   Client.on("guildMemberJoin", (guild, member) => reqEvent("guildMemberJoin")(guild, member));
+  Client.on("guildMemberLeave", (guild, member) => reqEvent("guildMemberLeave")(guild, member));
   Client.on("ready", () => reqEvent("ready")(Client));
 }
