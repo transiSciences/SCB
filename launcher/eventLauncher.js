b2be8a1f83bd14cfa91e4bf468fbf5d0d8076747
transiSciences <37506275+transiSciences@users.noreply.github.com>
2019-06-16 09:53:36 +0200

    Update eventLauncher.js

--- launcher/eventLauncher.js
+++ launcher/eventLauncher.js
@@ -1,5 +1,6 @@
 const reqEvent = (event) => require(`../events/${event}.js`);
 
 module.exports = Client => {
+  Client.on("guildMemberJoin", (guild, member) => reqEvent("guildMemberJoin")(guild, member));
   Client.on("ready", () => reqEvent("ready")(Client));
 }
